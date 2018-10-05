817f94574c1b7b225b59dce3f7148f0075d282e7
Yuan-Chia,Chang <k134563@gmail.com>
2018-10-05 21:02:34 +0800

    update

---
+++ Python-Data-Structures/Week4/Assignment 8.4.py
@@ -0,0 +1,15 @@
+fname = input("Enter file name: ")
+
+fh = open(fname)
+
+lst = list()
+
+for line in fh:
+	line = line.rstrip()
+	line = line.split()
+	for c in line:
+		if c in lst:
+			continue
+		else:
+            lst.append(c)
+print(line)
